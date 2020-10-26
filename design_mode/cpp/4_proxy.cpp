9acbe4ae3b96248547921d33e7305058bcdf3573
dangwei <dangwei@360.cn>
2020-10-26 22:10:35 +0800

    [proxy] add note

--- design_mode/cpp/4_proxy.cpp
+++ design_mode/cpp/4_proxy.cpp
@@ -7,6 +7,29 @@
 
 using namespace std;
 
+// 代理模式
+// 抽象类，包含功能接口
+// 实现类，继承抽象类，并实现功能
+// 代理类，集成抽象类，定义实现类的成员变量，通过实现类的对象，来调用具体实现功能的函数
+
+
+// 该类仅为满足用例，可不存在
+class SchoolGirl
+{
+public:
+	void setname(string name) {
+		name_ = name;
+	}
+
+	string getname() {
+		return name_;
+	}
+
+private:
+	string name_;
+};
+
+// 礼物接口
 class CGiveGift
 {
 public:
@@ -15,73 +38,62 @@ public:
 	virtual void GiveChocolates() = 0;
 };
 
-class SchoolGirl
-{
-public:
-	void setname(string name) {
-		_name = name;
-	}
-
-	string getname() {
-		return _name;
-	}
-
-private:
-	string _name;
-};
-
+// 追求者类，具体实现了礼物接口
 class Pursuit : public CGiveGift {
 public:
 	Pursuit(SchoolGirl* schoolgirl) {
-		_schoolgirl = schoolgirl;
+		schoolgirl_ = schoolgirl;
 	}
 
 	void GiveDolls() {
-		cout << "pursuit give dolls to "<< _schoolgirl->getname() << "\n";
+		cout << "pursuit give dolls to "<< schoolgirl_->getname() << "\n";
 	}
 
 	void GiveFollows() {
-		cout << "pursuit give follows to " << _schoolgirl->getname() << "\n";
+		cout << "pursuit give follows to " << schoolgirl_->getname() << "\n";
 	}
 
 	void GiveChocolates() {
-		cout << "pursuit give chocolates to " << _schoolgirl->getname() << "\n";
+		cout << "pursuit give chocolates to " << schoolgirl_->getname() << "\n";
 	}
 
 private:
-	SchoolGirl* _schoolgirl;
+	SchoolGirl* schoolgirl_ = nullptr;
 };
 
+// 代理类，内部定义具体实现类的对象，在继承的函数中调用
 class Proxy : public CGiveGift {
 public:
 	Proxy(SchoolGirl* schoolgirl) {
-		_pursuit = new Pursuit(schoolgirl);
+		pursuit_ = new Pursuit(schoolgirl);
 	}
 	
 	void GiveDolls() {
-		_pursuit->GiveDolls();
+		pursuit_->GiveDolls();
 	}
 
 	void GiveFollows() {
-		_pursuit->GiveFollows();
+		pursuit_->GiveFollows();
 	}
 
 	void GiveChocolates() {
-		_pursuit->GiveChocolates();
+		pursuit_->GiveChocolates();
 	}
 
 
 private:
-	Pursuit* _pursuit;
+	Pursuit* pursuit_ = nullptr;
 };
 
 
-int main4()
+int main()
 {
 	SchoolGirl* schoolgirl = new SchoolGirl();
 	schoolgirl->setname("李娇娇");
 
+	// 代理对象
 	Proxy* proxy = new Proxy(schoolgirl);
+	// 代理对象中调用具体实现类的对象的实现方法
 	proxy->GiveDolls();
 	proxy->GiveFollows();
 	proxy->GiveChocolates();
