c01ab520a8275230c98d5e2813f977c968056e94
dangwei <dangwei@360.cn>
2020-11-04 23:00:50 +0800

    [flyweight] 1.add note. 2.fix memory leak.

--- design_mode/cpp/21_mediator.cpp
+++ design_mode/cpp/21_mediator.cpp
@@ -91,7 +91,7 @@ private:
   ConcreteColleagueB* colleague_b_ = nullptr;
 };
 
-int main()
+int main21()
 {
   ConcreteMediator* mediator = new ConcreteMediator();
 
--- design_mode/cpp/22_flyweight.cpp
+++ design_mode/cpp/22_flyweight.cpp
@@ -1,18 +1,24 @@
- // 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-// 参考大话设计模式
+// 参考大话设计模式 - 享元模式
 
 #include <iostream>
 #include <vector>
 
+#ifndef SAFE_DELETE
+#define SAFE_DELETE(p) { if(p){delete(p); (p)=NULL;} }
+#endif
+
 using namespace std;
 
+// 方法接口 抽象类
 class Flyweight {
 public:
   virtual void Operation(int num) = 0;
 };
 
+// 可共用的方法实现
 class ConreteFlyweight : public Flyweight {
 public:
   void Operation(int num) {
@@ -20,6 +26,7 @@ public:
   }
 };
 
+// 不可共用的方法实现
 class UnsharedConreteFlyweight : public Flyweight {
 public:
   void Operation(int num) {
@@ -30,7 +37,17 @@ public:
 class FlyweightFactory {
 public:
   FlyweightFactory() {
+    // 写法不好，不易内存管理
     flyweight_.push_back(new ConreteFlyweight());
+  }
+
+  ~FlyweightFactory()
+  {
+    for (auto it = flyweight_.begin(); it != flyweight_.end(); it++)
+    {
+      SAFE_DELETE(*it);
+    }
+    flyweight_.clear();
   }
 
   Flyweight* GetFlyweight() {
@@ -42,7 +59,7 @@ private:
   vector<Flyweight*> flyweight_;
 };
 
-int main22()
+int main()
 {
   FlyweightFactory* flyweight_factory = new FlyweightFactory();
 
@@ -54,6 +71,9 @@ int main22()
 
   UnsharedConreteFlyweight* unshared_concrete_flyweight = new UnsharedConreteFlyweight();
   unshared_concrete_flyweight->Operation(16);
+
+  SAFE_DELETE(unshared_concrete_flyweight);
+  SAFE_DELETE(flyweight_factory);
 
 	return 0;
 }
