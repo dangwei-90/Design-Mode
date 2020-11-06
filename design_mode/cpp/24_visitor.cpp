e2e4d50c9aafc942d37af736f58f6830b15ea55a
dangwei <dangwei@360.cn>
2020-11-06 11:13:49 +0800

    [visitor] use shared_ptr instead of SAFE_DELETE

--- design_mode/cpp/24_visitor.cpp
+++ design_mode/cpp/24_visitor.cpp
@@ -25,7 +25,7 @@ public:
 
 class Person {
 public:
-  virtual void Accept(Action* action) = 0;
+  virtual void Accept(std::shared_ptr<Action> action) = 0;
 };
 
 class Success : public Action {
@@ -63,50 +63,68 @@ public:
 
 class Man :public Person {
 public:
-  void Accept(Action* action) {
+  void Accept(std::shared_ptr<Action> action) {
     action->GetManConclusion(this);
   }
 };
 
 class Women :public Person {
 public:
-  void Accept(Action* action) {
+  void Accept(std::shared_ptr<Action> action) {
     action->GetWomenConlusion(this);
   }
 };
 
+// use unique_ptr, not support our demo.
+void display(vector<std::unique_ptr<Person>>& person, std::unique_ptr<Action> action) {
+  for (auto it = person.begin(); it != person.end(); it++) {
+    (*it)->Accept(std::move(action));
+  }
+}
+
 int main()
 {
-  vector<Person*> person;
-  person.push_back(new Man());
-  person.push_back(new Women());
+  // use shared_ptr instead of new point.
+  //vector<Person*> person;
+  //person.push_back(new Man());
+  //person.push_back(new Women());
+
+  vector<std::shared_ptr<Person>> person;
+  person.push_back(std::shared_ptr<Man>(new Man()));
+  person.push_back(std::shared_ptr<Women>(new Women()));
   
-  Success* success = new Success();
+  // Success* success = new Success();
+  std::shared_ptr<Success> success(new Success());
+  //display(person, std::move(success));
+
   for (auto it = person.begin(); it != person.end(); it++)
   {
     (*it)->Accept(success);
   }
 
-  Fail* fail = new Fail();
+  // Fail* fail = new Fail();
+  std::shared_ptr<Fail> fail(new Fail());
   for (auto it = person.begin(); it != person.end(); it++)
   {
     (*it)->Accept(fail);
   }
 
-  Amtiveness* amtiveness = new Amtiveness();
+  // Amtiveness* amtiveness = new Amtiveness();
+  std::shared_ptr<Amtiveness> amtiveness(new Amtiveness());
   for (auto it = person.begin(); it != person.end(); it++)
   {
     (*it)->Accept(amtiveness);
   }
 
+  // use shared_ptr. we need not delete vector.
   // delete person
-  for (auto it = person.begin(); it != person.end(); it++) {
-    SAFE_DELETE(*it);
-  }
+  //for (auto it = person.begin(); it != person.end(); it++) {
+  //  SAFE_DELETE(*it);
+  //}
 
-  SAFE_DELETE(success);
-  SAFE_DELETE(fail);
-  SAFE_DELETE(amtiveness);
+  // SAFE_DELETE(success);
+  // SAFE_DELETE(fail);
+  // SAFE_DELETE(amtiveness);
 
 	return 0;
 }
